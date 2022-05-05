f527fd62e59502d0ed4a2095957a377b1fd27416
Alex-Ostrovny <alexandr.ostrovny@wesoftyou.com>
2022-05-05 10:44:44 +0300

    Add telegram notification

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -18,15 +18,26 @@ import { UserEmail } from '../decorators/user-email.decorator';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewService } from './review.service';
+import { TelegramService } from '../telegram/telegram.service';
 
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+  constructor(
+    private readonly reviewService: ReviewService,
+    private readonly telegramService: TelegramService,
+  ) {}
 
   @UsePipes(new ValidationPipe())
   @Post('create')
   async create(@Body() dto: CreateReviewDto) {
     return this.reviewService.create(dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post('notify')
+  async notify(@Body() dto: CreateReviewDto) {
+    const message = `Name: ${dto.name}\nTitle: ${dto.title}\nDescription: ${dto.description}\nRating: ${dto.rating}\nProduct ID: ${dto.productId}`;
+    return this.telegramService.sendMessage(message);
   }
 
   @UseGuards(JwtAuthGuard)
